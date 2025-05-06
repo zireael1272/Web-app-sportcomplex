5d50b119de03a1e0283e5664037a0895cabb9454
zireael1272 <kseniagonsharenko2007@gmail.com>
2025-05-06 22:56:10 +0300

    Add profile data

--- sportcenter_db_users.sql
+++ sportcenter_db_users.sql
@@ -37,7 +37,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'user','user123','user'),(2,'admin','admin890','admin');
+INSERT INTO `users` VALUES (1,'user','user123','user');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-04 22:06:06
+-- Dump completed on 2025-05-06 22:55:03
